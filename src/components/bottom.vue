05840a8b220402c4aea0c551f5c78e1b460253b4
iMocco <taoxin167@gmail.com>
2017-06-14 11:24:45 +0800

    引入fontawesome图标库 修改底部tab组件

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -1,19 +1,78 @@
 <template>
-	<div class="hello">
-		{{msg}}
+	<div class="e-bottom">
+		<div class="e-bottom-item">
+			<ul>
+				<li v-for="(item, index) in items" :style="{width:width+'%'}" :class="current === index ? 'current':''" @click="current=index">
+					<i class="fa " :class="item.icon"></i>
+					<p>{{item.text}}</p>
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'hello',
+		name: 'bottom',
+		props:{
+			value: {
+				type: Array,
+				default: false
+			},
+			current: {
+				type: Number,
+				default: 0
+			}
+		},
 		data () {
 			return {
-				msg: 'Welcome to Your Vue.js App'
+				items: this.value,
+				width: 100 / this.value.length
+			}
+		},
+		watch: {
+			value (val) {
+				this.items = val;
+				this.width = 100 % val.length;
 			}
 		}
+
 	}
 </script>
 
-<style scoped>
+<style scoped lang="less">
+	.e-bottom{
+		width: 100%;
+		position: absolute;
+		bottom: 0;
+		overflow: hidden;
+		.e-bottom-item{
+			width: 100%;
+			border-top: 1px solid;
+			ul{
+				padding: 0;
+				margin-top: 5px;
+				height: 25px;
+				li{
+					padding: 0;
+					list-style: none;
+					float: left;
+					text-align: center;
+					color: #666;
+					p{
+						font-size: small;
+						padding: 0;
+						margin: 0;
+					}
+				}
+				.current{
+					color:#000;
+				}
+				li:last-child{
+					border: none;
+				}
+			}
+
+		}
+	}
 </style>
--- src/index.vue
+++ src/index.vue
@@ -1,16 +1,28 @@
 <template>
 	<div>
-		ddad
+		<bottom v-model="value" :current.sync="current"></bottom>
 	</div>
 </template>
 <script>
-export default {
-  name: 'index',
-  data () {
-    return {
-    }
-  }
-}
+	import bottom from '@/components/bottom.vue'
+	export default {
+		components:{
+			bottom
+		},
+		name: 'index',
+		data () {
+			return {
+				value:[
+				{ text:'精选', icon:'fa-home' },
+				{ text:'分类', icon:'fa-server' },
+				{ text:'杂志', icon:'fa-paper-plane' },
+				{ text:'购物车', icon:'fa-tags' },
+				{ text:'我的', icon:'fa-user-o' },
+				],
+				current:0
+			}
+		}
+	}
 </script>
 
 <style scoped>
