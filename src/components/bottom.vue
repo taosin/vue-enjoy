c774c23b4f7a66dbd0256cdabf9bcd1e4e97109b
iMocco <taoxin167@gmail.com>
2017-06-14 09:53:20 +0800

    修改配置

---
+++ src/components/bottom.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="hello">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'hello',
+		data () {
+			return {
+				msg: 'Welcome to Your Vue.js App'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="hello">
-  </div>
+	<div>
+		ddad
+	</div>
 </template>
-
 <script>
 export default {
-  name: 'hello',
+  name: 'index',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
